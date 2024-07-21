8aeec59bfd1d9a77b996d58134b921d2ca96a9c1
Vidhee Agrawal <agrawalvr_1@rknec.edu>
2024-07-21 11:39:41 +0530

    Your commit message

---
+++ src/api/index.js
@@ -0,0 +1,12 @@
+const baseURL =
+  "https://api.weatherapi.com/v1/current.json?key=ba37fb3a589245c794145032242107%20";
+
+export const getWeatherDataForCity = async (city) => {
+  const response = await fetch(`${baseURL}&q=${city}&aqi=yes`);
+  return await response.json();
+};
+
+export const getWeatherDataForLocation = async (lat, lon) => {
+  const response = await fetch(`${baseURL}&q=${lat},${lon}&aqi=yes`);
+  return await response.json();
+};
